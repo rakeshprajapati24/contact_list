69f599c78338cd422ff6a16f38c7d14b8680ee44
Rakesh Prajapati <rakesh.prajapati@theadroit.in>
2023-12-28 14:35:00 +0530

    some changes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,13 @@
 class Contact < ApplicationRecord
+
+  validates :name,:contact_no,
+            :presence => {:message => 'Please enter'}
+  validates :name,
+            :format=> { with: /\A[a-zA-Z ]+\z/, 
+            :message=> "Only Characters are allowed" }
+  validates :contact_no,
+            :presence => {:message => 'Enter valid number'},
+            :numericality => true,
+            :length => { :minimum => 10, :maximum => 10,
+            :message=> "Enter valid number" }
 end
